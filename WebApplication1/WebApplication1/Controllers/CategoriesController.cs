2bd6f858c9f88ef15372d2628f51385fb70f77c0
Superyou <newtonysuperyou@gmail.com>
2022-11-21 06:31:01 +0900

    對category進行Scafold產出VIEW 並把邏輯移至service 降低耦合

---
+++ WebApplication1/WebApplication1/Controllers/CategoriesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Entity;
+using inter;
+using inter.Service;
+
+namespace inter.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly NorthwindContext _context;
+        private readonly CategoryService _CS;
+
+        public CategoriesController(CategoryService cS)
+        {
+            _CS = cS;
+        }
+
+        public CategoriesController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public IActionResult Index()
+        {
+            
+            return View(_CS.GetAllCategory());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _CS.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoryId,CategoryName,Description,Picture")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _CS.Create(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _CS.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,CategoryName,Description,Picture")] Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _CS.Update(category);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _CS.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _CS.Delete(await _CS.GetById(id));
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _CS.Any(id);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Service/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationCore.Entity;
+using ApplicationCore.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inter.Service
+{
+    public class CategoryService
+    {
+        private readonly IRepository<Category> _categoryRepo;
+
+        public CategoryService(IRepository<Category> categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+        public IQueryable<Category> GetAllCategory()
+        {
+            return _categoryRepo.GetAll();
+        }
+        public async Task<Category> GetById(int? id)
+        {
+            var category =await _categoryRepo.GetByIdAsync(id);
+            return category;
+        }
+        public async Task Create(Category category)
+        {
+            await _categoryRepo.AddAsync(category);
+        }
+        public async Task Update(Category category)
+        {
+            await _categoryRepo.UpdateAsync(category);
+        }
+        public async Task Delete(Category category)
+        {
+            await _categoryRepo.DeleteAsync(category);
+        }
+        public bool Any(int id)
+        {
+            return _categoryRepo.GetById(id) != null ;
+        }
+    }
+}
